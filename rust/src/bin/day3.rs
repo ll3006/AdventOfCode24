6b46495b72dfbdaee2846c59b6d28dcb96cd2454
ll3006 <doublel3006@gmail.com>
2024-12-14 06:39:36 +0100

    Add rust day3 part2 alt version

--- rust/src/bin/day3.rs
+++ rust/src/bin/day3.rs
@@ -64,8 +64,117 @@ fn part_two_regex() {
     println!("Multiplication sum (\\w conditionals) is {mul_sum}");
 }
 
+struct Stream<'a> {
+    string: &'a str,
+    position: usize
+}
+
+impl<'a> Stream<'a> {
+    pub fn new(string: impl Into<&'a str> ) -> Self {
+        Self { string: string.into(), position: 0 }
+    }
+    
+    fn _peek(&self, count: usize) -> (usize, &'a str) {
+        let string = self.string;
+        let start = self.position;
+        let end = 
+            string[start..]
+            .char_indices()
+            .nth(count)
+            .map_or(string.len(), |(idx, _)|  start + idx);
+    
+        (end, &string[start..end])
+        
+    }
+    pub fn peek(&mut self, count: usize) -> &'a str {
+        let (_, slice) = self._peek(count);
+        slice
+    }
+    
+    pub fn take(&mut self, count: usize) -> &'a str {
+        let (end, slice) = self._peek(count);
+        self.position = end;
+        slice
+
+    }
+    
+    pub fn skip(&mut self, count: usize) {
+        let (end, _) = self._peek(count);
+        self.position = end;
+    }
+    
+    pub fn over(&self) -> bool {
+        self.position >= self.string.len()
+    }
+    
+    pub fn next(&mut self) -> &str {
+        self.take(1)
+    }
+    
+    // pub fn restore(&mut self, pos: usize) {
+    //     self.position = pos;
+    // }
+}
+
+fn part_two() {
+    const MUL_OPEN: &str = "mul(";
+    const DO: &str = "do()";
+    const DONT: &str = "don't()";
+    
+    let contents = input();
+    let mut stream = Stream::new(contents.as_str());
+
+    let mut enabled = true;
+    let mut tot_mult = 0;
+    while !stream.over() {
+        match stream.peek(7) {
+            t if t.starts_with(MUL_OPEN) => {
+                stream.skip(MUL_OPEN.len());
+                let mut digit1 = String::new();
+                let mut digit2 = String::new();
+                let mut found_comma = false;
+                let mut completed = false;
+                while !completed {
+                    match stream.next() {
+                        t if t.chars().next().map_or(false, char::is_numeric) => {
+                            let digit = if found_comma { &mut digit2 } else { &mut digit1 };
+                        digit.push_str(t);
+                        },
+                        "," if !digit1.is_empty() && !found_comma => {
+                            found_comma = true;
+                        }
+                        ")" if !digit1.is_empty() && found_comma && !digit2.is_empty() => {
+                            completed = true;
+                        }
+                        _ => { break; }
+                    }
+                }
+                if completed && enabled {
+                    println!("{digit1}, {digit2}");
+                    let digit1: i64 = digit1.parse().unwrap();
+                    let digit2: i64 = digit2.parse().unwrap();
+                    tot_mult += digit1 * digit2; 
+                }
+            },        
+            t if t.starts_with(DO) => {
+                stream.skip(DO.len());
+                enabled = true;
+                
+            }, 
+            t if t.starts_with(DONT) => {
+                stream.skip(DONT.len());
+                enabled = false;
+            },
+            _ =>  {
+                stream.skip(1);
+            } 
+        }
+    }
+    println!("Multiplication sum (\\w conditionals) is {tot_mult}");
+}
 
 fn main() {
     part_one_regex();
     part_two_regex();
+    part_two();
 }
